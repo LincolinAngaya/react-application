6b42cd8bb24af97789224bf913b0efc831802313
LincolinAngaya <lincolinangaya5626@gmail.com>
2022-07-11 16:34:55 +0300

    add tasks component

---
+++ my-react-app/src/components/Tasks.js
@@ -0,0 +1,28 @@
+const tasks=[
+  {
+  id:1,
+  text:'Doctor Appoitnment',
+  day:'Feb 5th at 2:30pm',
+  reminder:true,
+  },
+  {
+    id:2,
+    text:'Meeting At School',
+    day:'Feb 6th at 1:30pm',
+    reminder:true,
+    },
+    {
+      id:3,
+      text:'Food Shopping',
+      day:'Feb 5th at 2:30pm',
+      reminder:false,
+      }
+]
+
+const Task = () => {
+  return (
+    <div>Task</div>
+  )
+}
+
+export default Task
