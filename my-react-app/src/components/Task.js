70d06f7df6a93668d00aa46e21a6ae97c7408bee
LincolinAngaya <lincolinangaya5626@gmail.com>
2022-07-11 17:34:46 +0300

    style the react cance icon

--- my-react-app/src/components/Task.js
+++ my-react-app/src/components/Task.js
@@ -4,7 +4,7 @@ import{FaTimes} from 'react-icons/fa'
 const Task = ({tasks}) => {
     return (
           <div className='task'>
-            <h3>{tasks.text}<FaTimes /></h3>
+            <h3>{tasks.text}<FaTimes style={{color:'red',cursor:'pointer'}} /></h3>
             <p>{tasks.day}</p>
 
         </div>
