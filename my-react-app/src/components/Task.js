1afbeb156557b893fda7a80d25e8861cdabc0964
LincolinAngaya <lincolinangaya5626@gmail.com>
2022-07-11 17:11:51 +0300

    add component Task to to render

---
+++ my-react-app/src/components/Task.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+const Task = () => {
+    return (
+          <div className='event'>
+            <h3>my task</h3>
+
+        </div>
+    )
+}
+
+export default Task;
