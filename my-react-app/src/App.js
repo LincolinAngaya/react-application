620e3bce0f3519678c1c58ead0e49bc02e000585
LincolinAngaya <lincolinangaya5626@gmail.com>
2022-07-11 17:06:19 +0300

    because tasks component can be reused, we prefer sending the array to the app coponent and destructure the task component

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,13 +1,36 @@
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import { useState } from "react";
 
 //import React from "react";---used when using class;
 function App() {
- 
+  const  [tasks, setTasks] = useState(
+    [
+      {
+      id:1,
+      text:'Doctor Appoitnment',
+      day:'Feb 5th at 2:30pm',
+      reminder:true,
+      },
+      {
+        id:2,
+        text:'Meeting At School',
+        day:'Feb 6th at 1:30pm',
+        reminder:true,
+        },
+        {
+          id:3,
+          text:'Food Shopping',
+          day:'Feb 5th at 2:30pm',
+          reminder:false,
+          }
+    ]
+  )
+
   return (
     <div className="container">
         <Header title='Task Tracker'/>
-        < Tasks />
+        < Tasks tasks={tasks} />
     
       
     </div>
