d61244f9d6213d1e2abe4c02c724bee1f0ba8a81
LincolinAngaya <lincolinangaya5626@gmail.com>
2022-07-11 16:50:05 +0300

    our tasks we can add them tom the useState and no difference it will still work

--- my-react-app/src/components/Tasks.js
+++ my-react-app/src/components/Tasks.js
@@ -1,25 +1,28 @@
-const tasks=[
-  {
-  id:1,
-  text:'Doctor Appoitnment',
-  day:'Feb 5th at 2:30pm',
-  reminder:true,
-  },
-  {
-    id:2,
-    text:'Meeting At School',
-    day:'Feb 6th at 1:30pm',
-    reminder:true,
-    },
-    {
-      id:3,
-      text:'Food Shopping',
-      day:'Feb 5th at 2:30pm',
-      reminder:false,
-      }
-]
+import { useState } from "react";
 
 const Tasks = () => {
+  const  [tasks, setTasks]=useState(
+    [
+      {
+      id:1,
+      text:'Doctor Appoitnment',
+      day:'Feb 5th at 2:30pm',
+      reminder:true,
+      },
+      {
+        id:2,
+        text:'Meeting At School',
+        day:'Feb 6th at 1:30pm',
+        reminder:true,
+        },
+        {
+          id:3,
+          text:'Food Shopping',
+          day:'Feb 5th at 2:30pm',
+          reminder:false,
+          }
+    ]
+  )
   return (
     <>
     {tasks.map((task) => (
